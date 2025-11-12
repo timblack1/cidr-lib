ed29ce2917e197bb67697431328eaae1e7997c1f
Tim Black <tim@alwaysreformed.com>
2025-11-12 01:39:32 -0800

    chore: switch to more manual config

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  branches: ['master'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/npm',
+      {
+        // Don't publish to npm, just update package.json version
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+}
