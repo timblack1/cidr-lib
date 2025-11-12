cc97d95359258432000af64b0af26023372483fc
Tim Black <tim@alwaysreformed.com>
2025-11-12 01:42:44 -0800

    chore: use exec

--- release.config.js
+++ release.config.js
@@ -3,17 +3,21 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    // This plugin will update the CHANGELOG.md file
+    '@semantic-release/changelog',
     [
       '@semantic-release/npm',
       {
-        // Don't publish to npm, just update package.json version
+        // This will only update the package.json version, not publish
         npmPublish: false,
       },
     ],
+    // This plugin will publish the package to npm using OIDC
+    ['@semantic-release/exec', { publishCmd: 'npm publish' }],
     [
       '@semantic-release/git',
       {
-        assets: ['package.json'],
+        assets: ['package.json', 'CHANGELOG.md'],
         message:
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
       },
